75a9190c0bb3582125eb334461ff4611353b7dbe
Jheovanny Camacho <jheocamacho@gmail.com>
2019-10-22 23:57:21 -0700

    added camera functionality
    - added camera/photo functionality
    - only works sometimes, other times recieve a MediaCaptureFailedEvent
    - for now, will only use captured image within the app itself, does not seem to save to camera roll
    - tested only on UWP

--- ImageRater/ImageRater/ImageRater.Android/Properties/AssemblyInfo.cs
+++ ImageRater/ImageRater/ImageRater.Android/Properties/AssemblyInfo.cs
@@ -32,3 +32,7 @@ using Android.App;
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+
+// Filter out devices that do not support required hardware
+[assembly: UsesFeature("android.hardware.camera", Required = true)]
+[assembly: UsesFeature("android.hardware.camera.autofocus", Required = true)]
--- ImageRater/ImageRater/ImageRater/MainPage.xaml.cs
+++ ImageRater/ImageRater/ImageRater/MainPage.xaml.cs
@@ -16,6 +16,16 @@ namespace ImageRater
 		public MainPage()
 		{
 			InitializeComponent();
-		}
-	}
+
+            CameraButton.Clicked += CameraButton_Clicked;
+        }
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+        }
+    }
 }
